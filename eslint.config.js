8011123fecce2e6494b8e70dfd99584fe7fea95e
tsmx <dev@tsmx.net>
2024-08-05 23:04:19 +0200

    Moved to eslint 9.8

---
+++ eslint.config.js
@@ -0,0 +1,29 @@
+const { configs } = require('@eslint/js');
+const jest = require('eslint-plugin-jest');
+
+let _test = 0;
+
+module.exports = [
+    configs.recommended,
+    {
+        languageOptions: {
+            ecmaVersion: 2018,
+            sourceType: 'commonjs',
+            globals: { Atomics: 'readonly', SharedArrayBuffer: 'readonly' }
+        },
+        rules: {
+            semi: 'error',
+            quotes: ['error', 'single'],
+            indent: ['error', 4, { 'SwitchCase': 1 }],
+            'no-unused-vars': ['warn', { 'varsIgnorePattern': '^_', 'args': 'after-used', 'argsIgnorePattern': '^_' }]
+        },
+        ignores: ['snippets/*', 'scripts/*']
+    },
+    {
+        files: ['tests/**'],
+        ...jest.configs['flat/recommended'],
+        rules: {
+            ...jest.configs['flat/recommended'].rules
+        }
+    }
+];
